ed6c19bd603567fee58fd68e37b4c8369e6e0173
Ali Heydari <ali4heydari@gmail.com>
2018-12-15 13:38:32 +0330

    Implement TruthTable

---
+++ QuineMcCluskey/LogicState.cs
@@ -0,0 +1,10 @@
+﻿namespace QuineMcCluskey
+{
+    public enum LogicState : byte
+
+    {
+        False = 0,
+        True = 1,
+        DontCare = 2
+    }
+}
---
+++ QuineMcCluskey/TruthTable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace QuineMcCluskey
+{
+    class TruthTable
+    {
+        public string[] Titles;
+        public List<List<LogicState>> InputStates;
+        public List<LogicState> OutputStates;
+
+        public TruthTable(string[] titles, List<List<LogicState>> inputStates, List<LogicState> outputStates)
+        {
+            this.Titles = titles;
+            this.InputStates = inputStates;
+            this.OutputStates = outputStates;
+        }
+
+        public void SetInputStates(List<List<LogicState>> newStates)
+        {
+            this.InputStates = newStates;
+        }
+
+        public void SetOutputStates(List<LogicState> newStates)
+        {
+            this.OutputStates = newStates;
+        }
+
+        public void SetOutputStatesToTrue()
+        {
+            int count = InputStates.Count;
+            this.OutputStates = new List<LogicState>();
+            for (int i = 0; i < count; i++)
+            {
+                this.OutputStates.Add(LogicState.True);
+            }
+        }
+
+    }
+}
